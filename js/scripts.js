6db9d1d2ee4acae6d9ccf521ede30321126e428a
Katrina Hirsch and Nhat Hoang <katrina@>
2016-08-08 15:17:58 -0700

    added jquery file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+// Add
 var add = function(number1, number2) {return (number1 + number2);};
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+// Subtract
+var subtract = function(number1, number2) {return (number1 - number2);};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+// Multiply
+var multiply = function(number1, number2) {return (number1 * number2);};
+var number1 = parseInt(prompt("Enter a number."));
+var number2 = parseInt(prompt("Enter anther number."));
+// Divide
+var divide = function(number1, number2) {return (number1 / number2);};
+var number1 = parseInt (prompt("Enter a number."));
+var number2 = parseInt (prompt("Enter another number."));
+//Result
+var result = add(number1, number2)
+alert(result)
